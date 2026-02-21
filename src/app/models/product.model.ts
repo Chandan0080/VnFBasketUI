ba9ba710eef4fa01c708fb456e07cd7b46d2aa41
Chandan <guptachandan8320@gmail.com>
2026-02-22 03:53:14 +0530

    "UI image upload changes"

--- src/app/models/product.model.ts
+++ src/app/models/product.model.ts
@@ -2,7 +2,7 @@ export interface Product {
     productId?: number;
     productName?: string;
     productDescription?: string;
-    imageUrl?: string;
+    productImage?: string;
     productPrice?: number;
     stockQuantity?: number;
     categoryId?: number;
