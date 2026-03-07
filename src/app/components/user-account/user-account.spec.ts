facbc376d60b046484d8bb3dfc152bb1a3b237e4
Chandan <guptachandan8320@gmail.com>
2026-03-07 23:52:21 +0530

    minor chang in spec file

--- src/app/components/user-account/user-account.spec.ts
+++ src/app/components/user-account/user-account.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { UserAccount } from './user-account';
+import { UserProfile } from './user-account';
 
 describe('UserAccount', () => {
-  let component: UserAccount;
-  let fixture: ComponentFixture<UserAccount>;
+  let component: UserProfile;
+  let fixture: ComponentFixture<UserProfile>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [UserAccount]
+      imports: [UserProfile]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(UserAccount);
+    fixture = TestBed.createComponent(UserProfile);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
