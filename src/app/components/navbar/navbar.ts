df9468163fac72fcd024b5371602a2ccb76cac8f
Chandan <guptachandan8320@gmail.com>
2026-03-07 23:47:08 +0530

    User modal

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth-service.service';
@@ -13,6 +13,7 @@ export class Navbar {
 
   isLoggedIn: boolean = false;
   role: string | null = null;
+  isAccountOpen = false;
 
   constructor(private authService: AuthService, private router: Router) {}
 
@@ -28,8 +29,17 @@ export class Navbar {
     
   }
 
-
   logout() {
     this.authService.logout();
   }
+
+  toggleAccountMenu(event: Event) {
+  event.stopPropagation(); // prevent document click closing immediately
+  this.isAccountOpen = !this.isAccountOpen;
+}
+
+@HostListener('document:click')
+closeMenu() {
+  this.isAccountOpen = false;
+}
 }
